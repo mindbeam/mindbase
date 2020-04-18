feca46a92c57713f49326bc892df744184fbbc9d
Daniel Norman <daniel@danielnorman.net>
2020-04-17 20:30:43 -0700

    quiesce warnings

--- src/genesis/language_en.rs
+++ src/genesis/language_en.rs
@@ -1,11 +1,10 @@
 use crate::{
-    artifact::Text,
     error::MBError,
     MindBase,
 };
 
 // TODO 2 - move this to MBQL, and load it up with lots of stuffs
-pub fn genesis(mb: &MindBase) -> Result<(), MBError> {
+pub fn genesis(_mb: &MindBase) -> Result<(), MBError> {
     // let _words = mb.put_artifact(Text::new("English words"))?;
 
     Ok(())
