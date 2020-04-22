11f96b7870b40327457c87d461736e30d37917b7
Daniel Norman <daniel@danielnorman.net>
2020-04-21 18:57:14 -0700

    MBQL next phase ideation

--- tests/apple.rs
+++ tests/apple.rs
@@ -6,29 +6,79 @@ fn apple() -> Result<(), MBError> {
     let tmpdirpath = tmpdir.path();
     let mb = MindBase::open(&tmpdirpath)?;
 
+    // Questions / notes:
+    // - Where precisely is the taxonomy?
+    // – How do we navigate this?
+    // - What *exactly* do $d, $k, $p etc represent? -- How do we bind to the desired subsymbol in a grounding? (No really: ?)
+    // - Something need to make the Token "weightier" when it comes time to render this, but why? --  Adding emphasis or schematic
+    //   annotation to this effect is clearly wrong, tantamount to putting one's thumb on the scale
+    // - How do we do N dimensional analogical grounding rather than 1 dimensional?
+    // - It seems we need some way to break associativity, and refer to the symbol itself -- .left creates a new composite symbol
+    //   of left-handed branch sub-symbols (abbreviated as <) -- .right creates a new composite symbol left-handed branch
+    //   sub-symbols (abbreviated as >) -- Thus $7.right.right (abbreviated as $7>>) should yield the unique symbol which is
+    //   tagged with "Malus Domestica" (and which may be queried by Ground("Token":"Type") to yeild the unique symbol tagged with
+    //   "Species" )
+
+    // TODO 1 - determine if subsymbol binding is compatible with resymbolizion in ground.rs - It should be
+    // TODO 1 - determine how we might be able to ground in multiple dimensions. Maybe Symbolvars need to perform lazy
+    // symbolization after all? Perhaps lazy symbolvar plus AND? Eg: Ground("Bar" : $serves_booze & !$restaurant)
+    // TODO 1 - what types of analogies do we want to represent?
+    //     catagorical ~
+    //     associative :
+    //     what others?
+    //     Eg: $pets = Ground(("Cat"? : "Kitty") : ("Dog"? : "Doggy"))
+    //     Eg: $shmoopy_pets = Ground(("Cat" : "Kitty"?) : ("Dog" : "Doggy"?))
+    //     Eg: ("Cat" ~ "Kitty") : ("Synonyms")
+    //     Eg: ("The tree fruit relationship as a single entity" ~ ("Tree" : "Fruit"))
+
     let query = mb.query_str(
                              r#"
-        $t = Ground("Type" : "Token")
-        $h = Ground("Holonym" : "Hyponym")
 
-        $d = Ground($t : ("Domain"  : "Eukarya"))
-        $k = Ground($t : ("Kingdom" : "Plantae"))
-        $p = Ground($t : ("Phylum"  : "Magnoliophyta"))
-        $c = Ground($t : ("Class"   : "Magnoliopsida"))
-        $o = Ground($t : ("Order"   : "Rosales"))
-        $f = Ground($t : ("Family"  : "Rosaceae"))
-        $g = Ground($t : ("Genus"   : "Malus"))
-        $s = Ground($t : ("Species" : "Malus domestica"))
+        # Alice has an apple
 
-        $1 = Ground( $h : ( $d : $k ) )
-        $2 = Ground( $h : ( $1 : $p ) )
-        $3 = Ground( $h : ( $2 : $c ) )
-        $4 = Ground( $h : ( $3 : $o ) )
-        $5 = Ground( $h : ( $4 : $f ) )
-        $6 = Ground( $h : ( $5 : $g ) )
-        $7 = Ground( $h : ( $5 : $s ) )
+        $tt = Ground("Type" : "Token")? # Trailing ? is the default if omitted
+        $hh = Ground("Holonym" : "Hyponym")?
+        $d = Ground( $tt : ("Domain"  : "Eukarya"?))
+        $k = Ground( $tt : ("Kingdom" : "Plantae"?))
+        $p = Ground( $tt : ("Phylum"  : "Magnoliophyta"?))
+        $c = Ground( $tt : ("Class"   : "Magnoliopsida"?))
+        $o = Ground( $tt : ("Order"   : "Rosales"?))
+        $f = Ground( $tt : ("Family"  : "Rosaceae"?))
+        $g = Ground( $tt : ("Genus"   : "Malus"?))
+        $s = Ground( $tt : ("Species" : "Malus domestica"?))
+        $1 = Ground( $hh : ( $d : $k? ) )
+        $2 = Ground( $hh : ( $1 : $p? ) )
+        $3 = Ground( $hh : ( $2 : $c? ) )
+        $4 = Ground( $hh : ( $3 : $o? ) )
+        $5 = Ground( $hh : ( $4 : $f? ) )
+        $6 = Ground( $hh : ( $5 : $g? ) )
+        $apple = Ground( $hh : ( $6 : $s? ) )
 
-        let $apple = Ground(("English Word" : "Apple") : $7 )
+        # Fruit is hyponymous of Tree _only_ when it's still attached
+        $tfhh_f = Ground(("Tree" : "Fruit"?) : $hh)
+        $tfpp_f = Ground(("Tree" : "Fruit"?) : ("Progenitor" : "Progeny"))
+
+        # Now we have a term that refers to fruit which may be ON or OF the tree
+        $fruit = $tfhh_f | $tfpp_f
+
+        # I think we need to have a syntax to navigate subsymbols even though ? binding is cleaner
+        $mdappletreefruit = Ground(($7>> : "Apple") : $tf)  # >> is abbreviated form of .right.right
+        $apple = $mdsappletreefruit<>                       # <> is abbreviated form of of .left.right
+
+        # Identical to the above
+        $apple2 = Ground(($7>> : "Apple"?) : $treefruit)
+        Assert($apple = $apple2)
+
+        $pos = Ground("Posessor":"Possession")
+        
+        # How do we do binding of ALL of $apple to $t<, and return a new symbol for $t>? 
+        $aa = Allege( "Alice" : ($t > *$apple) : $pos )
+
+        $personsaid = Ground("")
+        Allege($apple : $s>>)
+
+
+        ("English Word" : "Apple") : ("Species": "Malus Domestica" )
 
         # Not yet sure how to deal with these
         # $x = Ground($h : ("Ontological System" : "Biological Taxonomy"))
@@ -39,6 +89,9 @@ fn apple() -> Result<(), MBError> {
     query.apply()?;
 
     let apple = query.get_symbol_var("apple")?.unwrap();
+
+    // I have three apples
+    //
 
     // TODO 1 - fix this
 
